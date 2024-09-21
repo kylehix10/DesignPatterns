2cd6bada1483562df08f98be8dec817b80ef544e
KyleHix <khix@email.sc.edu>
2024-09-21 10:19:44 -0400

    Final

---
+++ singleton/Question.java
@@ -0,0 +1,65 @@
+//Kyle Hix 2024
+
+import java.util.ArrayList;
+
+/**
+ * Class outlines the properties of a question.
+ * Returns a question, an answer, and checks if the user is correct.
+ */
+
+public class Question {
+
+    private String question;
+    private ArrayList<String> answers;
+    private int correctAnswer;
+
+    /**
+     * Constructors of the Question class.
+     * @param question Storing the input of question to this question.
+     * @param correctAnswer Storing the index of the correct Answer. Given by the txt file
+     * @param answer An array of string. Multiple choice answers.
+     */
+    public Question(String question, int correctAnswer, String[] answer){
+        this.question=question;
+        answers=new ArrayList<String>();
+        for(int i=0; i<answer.length;i++){
+           String multipleChoice=answer[i];
+            answers.add(multipleChoice);
+        }
+        this.correctAnswer= correctAnswer;
+    }
+    /**
+     * Returns the question with answer choices in a multiple choice format. 
+     * Note: the choice option does not match the index.
+     */
+    public String toString(){
+        return 
+           "\n"+ question+"\n"+
+            "1. "+answers.get(0)+" "+
+            "2. "+answers.get(1)+" "+
+            "3. "+answers.get(2)+" "+
+            "4. "+answers.get(3);
+    }
+
+    /**
+     * Checks the users input against the correct answer index
+     * @param userAnswer 
+     * @return
+     */
+    public boolean isCorrect(int userAnswer){
+        int answer=userAnswer-1;
+        if(answer==correctAnswer)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * Pulling correct answer index from the array.
+     * @return
+     */
+    public String getCorrectAnswer(){
+       return answers.get(correctAnswer);
+    }
+    
+}
---
+++ singleton/TriviaGame.java
@@ -0,0 +1,93 @@
+//Kyle Hix 2024
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+/**
+ * Trivia game class that pulls questions, keeps score, and track of what round the user is on.
+ */
+
+public class TriviaGame {
+
+    private static TriviaGame triviaGame;
+    private int score;
+    private int numGames;
+    private Random rand;
+    private ArrayList<Question> questions;
+
+    /**
+     * TriviaGame constructors.
+     * Loading trivia questions from a text file into the ArrayList of questions.
+     */
+
+    private TriviaGame(){
+        score=0;
+        numGames=0;
+        rand = new Random();
+        questions=DataLoader.getTriviaQuestions();
+    }
+
+    /**
+     * Creates a new triviaGame
+     * @return a new TriviaGame
+     */
+    public static TriviaGame getInstance(){
+        if(triviaGame==null)
+            triviaGame=new TriviaGame();
+        return triviaGame;
+    }
+
+    /**
+     * Pulling a random question from the ArrayList based on a random index number.
+     * If the list is empty the game is over and displays the users results.
+     * @return random question from the Array List.
+     */
+    public Question getQuestion(){
+         if (questions.isEmpty()) {
+            System.out.println("Out of questions\nYou scored: "+score+"/"+numGames);
+            System.exit(0);
+        }
+        int questionNum= rand.nextInt(questions.size());
+        Question question = questions.get(questionNum);
+        questions.remove(questionNum);
+        return question;
+        
+      
+    }
+
+    /**
+     * Checks the arrayList of questions to see if it is empty or not.
+     * @return
+     */
+    public boolean hasMoreQuestions(){
+        if(questions.isEmpty()==true)
+            return false;
+        else
+            return true;
+    }
+
+    /**
+     * Increases the score and number of games played.
+     */
+    public void winRound(){
+        score++;
+        numGames++;
+    }
+
+    /**
+     * Increases the number of games played. No increase in score.
+     */
+    public void loseRound(){
+        numGames++;
+    }
+
+    /**
+     * Returns the score to games played to the user.
+     * @return score to games played.
+     */
+    public String getScores(){
+        return "You scored: "+score+"/"+numGames;
+    }
+
+}
